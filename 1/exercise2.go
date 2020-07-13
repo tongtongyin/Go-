e6b22903c90ade291ec75a23c4635f9ef9d65045
tongtongyin <37565148+tongtongyin@users.noreply.github.com>
2020-07-13 16:56:40 +0800

    Add files via upload

---
+++ 1/exercise2.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+// 修改echo程序，使其打印每个参数的索引和值，每个一行。
+func main() {
+	// var s, sep string
+	// 修改索引从0开始，即可打印被执行命令本身的名字
+	for i := 0; i < len(os.Args); i++ {
+		fmt.Println(i, os.Args[i])
+	}
+
+}
